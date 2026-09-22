08f5e2a778582830d3794a5f1c2dcf3367e2b4e8
hsay40 <50463206+hsay40@users.noreply.github.com>
2026-09-22 20:25:20 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int f(int index,vector<int> &dp){
-        if(dp[index]!=-1){return dp[index];}
-        int left = f(index-1,dp);
-        int right = f(index-2,dp);
-        return dp[index] =  left+right;
-    }
-
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        return f(n,dp);
+        int prev2 = 1;
+        int prev1 = 1;
+
+        for(int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 };
